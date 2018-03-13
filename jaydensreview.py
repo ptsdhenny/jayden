c3c2bf4c764c891571c7ac8f267579d11e539d95
Jayden Lee Jones <jayden.jones@stu.jefferson.kyschools.us>
2018-03-13 09:26:20 -0400

    "first comment"

---
+++ jaydensreview.py
@@ -0,0 +1,37 @@
+"""
+this program will have the user state a username and pass word of their choice
+this will enable the user to be welcomed by a greeting
+this will include a list of programs and data that is the users 
+"""
+username = input("what is your username?")
+
+password = input("what is your password?")
+
+print ("welcome user, would you like to search medias?")
+
+response = input("what is your response?")
+
+media_type = input("what is your media type?")
+
+media_title = input("what is the song name?")
+
+media_genre = input("what genre the is the song?")
+
+print ("Ok, this is what is have found listed under EVERYDAY song titles in HIP HOP")
+
+print ("A$AP Rocky - Everyday")
+
+print ("Logic - Everyday (ft marshmello)")
+
+print ("Ariana Grande - Everyday") 
+
+print ("The Deli - Everyday") 
+new_list = [media_title, media_type, media_genre]
+
+dubstep = []
+
+dubstep.append("Pumped up kicks")
+
+media_rating = input("what do you rate your song?")
+
+print ("I agree, that rating is suitable for that song")
