3aa328cf2000bd34703512da64e0133be9b32645
ptsdhenny <36766977+ptsdhenny@users.noreply.github.com>
2018-05-02 10:00:45 -0400

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+Ball_size = 8
+Ball_color = "Red" 
+Ball_text = "Supreme"
+Ball_style = "Kick ball"
+Ball_material = "Rubber"
+# ball_user = input("what would you like to do with this ball?")
+
+def Kick_ball(can_you_kick):
+    if can_you_kick == True:
+        print("Off it goes")
+    else:
+        print("you missed")
+Kick_ball(True)
+ 
